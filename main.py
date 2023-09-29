bd41058ab6c49ef05acf0df97da0389ba97c9cec
The Taha Yaseen <thetahayaseen@gmail.com>
2023-09-30 02:13:20 +0500

    The Connection To The Vector Database By Cassandra

---
+++ main.py
@@ -0,0 +1,17 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import json
+
+cloud_config= {
+  'secure_connect_bundle': 'secure-connect-aiadventuregamedatabase.zip'
+}
+
+with open("AIAdventureGameDatabase-token.json") as f:
+    secrets = json.load(f)
+
+CLIENT_ID = secrets["clientId"]
+CLIENT_SECRET = secrets["secret"]
+
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
